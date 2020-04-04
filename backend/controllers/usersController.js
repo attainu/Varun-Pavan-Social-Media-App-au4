123d7a107b82fcbb1198ddbe52b8a9e78accade6
Varun Inapakurthi <princevarun78@gmail.com>
2020-04-04 15:08:12 +0530

    Password encryption added!

--- backend/controllers/usersController.js
+++ backend/controllers/usersController.js
@@ -1,10 +1,16 @@
 const User = require('./../models/user');
 const jwt = require('jsonwebtoken')
 const catchAsync = require('./../utils/catchAsync');
+const bcryptjs = require('bcryptjs')
 
 exports.createUser = catchAsync(async (req, res, next) => {
   let { name, email, phone, password, gender, location, bio } = req.body;
-  let user = await User.create({ name, email, phone, password, gender, location, bio });
+
+  //Encrypt password
+  let salt = await bcryptjs.genSalt(10);
+  let hashPassword = await bcryptjs.hash(password, salt)
+
+  let user = await User.create({ name, email, phone, password: hashPassword, gender, location, bio });
   res.json({
     status: true,
     data: user
@@ -13,8 +19,11 @@ exports.createUser = catchAsync(async (req, res, next) => {
 
 exports.loginUser = catchAsync(async (req, res, next) => {
   let { email, password } = req.body;
-  let user = await User.findOne({ email, password });
-  if (user) {
+  let user = await User.findOne({ email });
+
+  //Decrypt password
+  let validPassword = await bcryptjs.compare(password, user.password)
+  if (validPassword) {
     const token = jwt.sign({ _id: user._id }, 'secretkey')
     res.header('auth-token', token).json({
       status: true,
